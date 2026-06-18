01510feee3c85dedff7b54d8eb3523e19c1210d1
shwigy <floressj04@icloud.com>
2026-06-18 01:28:45 -0400

    Prepped index.js for creating required functions and fetch API  functionalities

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+// all necessary DOM elements needed
+const button = document.getElementById("fetch-alerts");
+const input = document.getElementById("state-input");
+const alertsDisplay = document.getElementById("alerts-display");
+const errorMessage = document.getElementById("error-message");
+
+// listening for button clicks, initiating the whole flow
+button.addEventListener("click", () => {
+  const state = input.value.trim();
+
+  input.value = "";
+
+  fetchWeatherAlerts(state);
+});
